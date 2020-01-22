23bec413ddf2d7b7e3d051a3b543a67477b74400
luukkonen1991 <luukkonen1991@gmail.com>
2020-01-22 14:55:39 +0200

    controllers, utils, middleware, models and errorhandling worked

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}, on mode ${process.env.NODE_ENV}`);
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}, on mode ${process.env.NODE_ENV}`.yellow.bold);
+});
+
+
+// Handle unhandeled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server & exit process
+  server.close(() => process.exit(1));
 });
